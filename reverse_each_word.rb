00a7121cad5e764d86e7593f34c7e561ac750fc2
Mohammed Noori <kmanoorib@gmail.com>
2018-05-22 22:07:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,17 @@
 def reverse_each_word (sentence)
+  
+  array = sentence.split
+  reversed_arr = []
+  
+  array.each do |word|
+    word.reverse
+    reversed_arr.push(word)
+  end
+  return reversed_arr.join
+=begin
   array = sentence.split
   array.map do |word|
     word.reverse
   end
+=end
 end
